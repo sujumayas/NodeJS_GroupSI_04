d1dede471889a79b213503ec83cdb2658c9a04ff
Alumno <alumno@A51-MM-16.local>
2016-11-26 12:20:20 -0500

    Clase01

---
+++ Clase01/08-credenciales.js
@@ -0,0 +1,6 @@
+var credenciales = require("./credenciales")
+
+console.log("Facebook")
+console.log("=========")
+console.log(credenciales.credenciales.facebook)
+console.log(credenciales.refrescoTiempo)
