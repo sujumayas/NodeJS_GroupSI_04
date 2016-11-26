2d1565f7fd3e7151cc9ee5132482d1e35613d77b
Alumno <alumno@A51-MM-16.local>
2016-11-26 12:49:34 -0500

    Clase01

---
+++ Clase01/09-servidor.js
@@ -0,0 +1,25 @@
+var http = require("http").createServer()
+
+function fnServidor(req, res){
+	res.writeHead(200, {"content-type": "text/plain"})
+	res.write("Todo está bien.")
+	res.end()
+}
+
+function fnEscuchando(){
+	console.log("Servidor escuchando en el puerto " + (process.env.port || 3000))
+}
+
+http
+	.on("request", fnServidor)
+	.listen(process.env.port || 3000, fnEscuchando)
+
+/*http.on("request", function(req, res){
+	res.writeHead(200, {"content-type": "text/plain"})
+	res.write("Todo está bien.")
+	res.end()
+})
+
+http.listen(3000, function(){
+	console.log("Servidor escuchando en el puerto 3000")
+})*/
