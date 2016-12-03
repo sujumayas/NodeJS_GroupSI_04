1a107bf2a6e901b181f9e384357a2427a4c4333a
Admin <admin@A51-MM-16.local>
2016-12-03 11:18:17 -0500

    Clase02

--- Clase02/app02/index.js
+++ Clase02/app02/index.js
@@ -5,14 +5,18 @@ function fnEscuchando(){
 	console.log("Escuchando en el puerto 3000")
 }
 
-var app = require("express")(),
+var express = require("express"),
+	app = express(),
 	path = require("path"),
 	motorVistas = "ejs",
-	dirVistas = "/vistas"
+	dirVistas = "/vistas",
+	dirPublico = "/publico"
 
 
 app.set("view engine", motorVistas)
 app.set("views", path.join(__dirname, dirVistas))
+
+app.use(express.static(__dirname + dirPublico))
 
 app.listen(3000, fnEscuchando)
 
@@ -37,7 +41,26 @@ app.get("/descargar", function(req, res){
 })
 
 app.get("/listado", function(req, res){
-	res.render("listado")
+	var datos = {
+		nombre: "Sergio",
+		apellido: "Hidalgo"
+	}
+	res.render("listado", datos)
+})
+
+app.get("/usuarios", function(req, res) {
+
+	var registros = [
+		{nombre: "Nombre1", apellido: "Apellido1"},
+		{nombre: "Nombre2", apellido: "Apellido2"},
+		{nombre: "Nombre3", apellido: "Apellido3"},
+		{nombre: "Nombre4", apellido: "Apellido4"},
+		{nombre: "Nombre5", apellido: "Apellido5"}
+	]
+
+	res.render("usuarios", {
+		registros: registros
+	})
 })
 
 
