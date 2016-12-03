6bc2fdea453e20e952e26a4f4844adadb8c387ce
Admin <admin@A51-MM-16.local>
2016-12-03 14:28:50 -0500

    Clase02

--- Clase02/app03/routes/usuarios.js
+++ Clase02/app03/routes/usuarios.js
@@ -1,15 +1,55 @@
 var express = require("express"),
 	router = express.Router(),
 	mongodb = require("mongodb"),
-	db = require("monk")("localhost/bdusuarios")
+	db = require("monk")("localhost/bdusuarios"),
+	Usuarios = db.get("usuarios")
 
 router.get("/", function(req, res, next){
-	var Usuarios = db.get("usuarios")
-
 	Usuarios
 		.find()
 		.then(function(registros){
 			res.render("lista-usuarios", {registros: registros})
+		})
+		.catch(function(error){
+			res.status(500)
+			res.send(error)
+		})
+})
+
+router.post("/", function(req, res, next){
+	var registro = {
+		nombre: req.body.nombre,
+		apellido: req.body.apellido
+	}
+
+	Usuarios
+		.insert(registro)
+		.then(function(registro){
+			res.redirect("/usuarios")
+		})
+		.catch(function(error){
+			res.status(500)
+			res.send(error)
+		})
+})
+
+router.get("/:id", function(req, res, next){
+	Usuarios
+		.find({_id: req.params.id})
+		.then(function(registros){
+			res.render("edicion", {registro: registros[0]})
+		})
+		.catch(function(error){
+			res.status(500)
+			res.send(error)
+		})
+})
+
+router.put("/:id", function(req, res, next){
+	Usuarios
+		.update({_id: req.params.id}, {nombre:req.body.nombre, apellido: req.body.apellido})
+		.then(function(registros){
+			res.redirect("/usuarios")
 		})
 		.catch(function(error){
 			res.status(500)
