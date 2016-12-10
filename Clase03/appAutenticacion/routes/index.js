72e3dfecdcd7b4fb2b0b44c5539e8f00224e32d9
Admin <admin@A51-MM-16.local>
2016-12-10 11:44:56 -0500

    Clase03

--- Clase03/appAutenticacion/routes/index.js
+++ Clase03/appAutenticacion/routes/index.js
@@ -10,12 +10,21 @@ function fntAutenticado(req, res, next){
 }
 
 /* GET home page. */
-router.get('/', fntAuntenticado, function(req, res, next){
+router.get('/', fntAutenticado, function(req, res, next){
 	res.render("home")
 });
 
 router.get("/login", function(req, res, next){
 	res.render("login")
+})
+
+router.post("/usuario", function(req, res, next){
+	if(req.body.usuario==="sergio" && req.body.contrasena==="1234") {
+		req.session.usuario = {usuario: req.body.usuario}
+		res.redirect("/")
+	} else {
+		res.render("login")
+	}
 })
 
 module.exports = router;
